23802cd512867e0ca20170989d049971e204546c
Orialm-A <orialm-asteria@protonmail.com>
2026-03-22 10:33:35 +0100

    [ fix ] Use type PathBuf instead of String for levels' path in structures related to generation layout and project toml

--- src/cli/resolution.rs
+++ src/cli/resolution.rs
@@ -183,7 +183,7 @@ pub fn resolve_module_details(parameter_details: Option<String>) -> PomResult<St
 pub fn resolve_module_level(
     level_parameter: Option<String>,
     project_levels_map: &ModuleLevelsMap,
-) -> PomResult<(String, Option<String>, String)> {
+) -> PomResult<(PathBuf, Option<String>, String)> {
     if let Some(level_parameter) = level_parameter
         && let Some(level_spec) = project_levels_map.get(&level_parameter)
     {
